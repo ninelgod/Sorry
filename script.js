c9fa1babc8a7f25253bf106df46c74d0dc834eed
Emilio Alfaro <137736967+ninelgod@users.noreply.github.com>
2025-06-22 21:47:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -29,12 +29,12 @@ function closeModal() {
   overlay.style.display = "none";
 }
 
-
-
+//Si quieres que al cerrar le redirija a tu chat de WhatsApp con un mensaje, solo quita el commit, reemplaza tu número y deja que el código haga lo suyo...
+/*
 function closeModal() {
   const numero = "Aquí va tu número de WhatsApp"; // Reemplaza con tu número de WhatsApp y listo :) ft NINELGOD
   const mensaje = encodeURIComponent("Te perdono, eres lo mejor que me ha pasado ❤️ Te quiero.");
   const url = `https://wa.me/${numero}?text=${mensaje}`;
 
   window.location.href = url;
-}
+}*/
